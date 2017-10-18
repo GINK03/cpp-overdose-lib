efab8b29ae37e6ce76ad0a214626108f174bac53
nardtree <gim.kobayashi@gmail.com>
2017-10-18 12:21:37 +0900

    update

--- main.cpp
+++ main.cpp
@@ -215,96 +215,26 @@ void mapperIndexedTest() {
 
 #include "Reflection.hpp"
 
-template <class T0, class T>
-std::string dump(const T* t, const T0* t0) {
-  int64_t basePosition = reinterpret_cast<int64_t>(t0);
-  int64_t memoryPosition = reinterpret_cast<int64_t>(t);
-  auto p = reinterpret_cast<unsigned char const*>(t);
-  std::ostringstream stream;
-  if constexpr (std::is_same_v<T, std::string>) {
-    stream << t->c_str();
-  } else {
-    for (size_t n = 0 ; n < sizeof(T) ; ++n) 
-      stream << std::setw(2) << std::setfill('0') << int(p[n]) << " ";
-  }
-  std::string type;
-  if constexpr (std::is_same_v<T, int>) {
-    type = "int";
-  }
-  if constexpr (std::is_same_v<T, double>) {
-    type = "double";
-  }
-  if constexpr (std::is_same_v<T, std::string>) {
-    type = "std::string";
-  }
-  std::ostringstream result_stream;
-  result_stream  << type << "<Left>" <<  memoryPosition-basePosition  << "<Left>" << stream.str() << "<Left>" << sizeof(T);
-  
-  return result_stream.str();
-}
 #include <any>
 #include <string.h>
 
 struct Sample {
   int a;
   double b;
-  std::string c; 
-
-  auto posiParam() {
-    cout << dump(&a,this) + "<Right>" + dump(&b,this) + "<Right>" + dump(&c,this) << endl;
+  std::string c;
+  std::string serial() {
+    return OD::SERIAL::dump(&a,this) + OD::SERIAL::SEPARATOR + OD::SERIAL::dump(&b,this) + OD::SERIAL::SEPARATOR + OD::SERIAL::dump(&c,this);
   };
 };
 
-//lexical recovery
-void recoverTest() {
-  string input = "int<Left>0<Left>01 00 00 00 <Left>4<Right>double<Left>8<Left>00 00 00 00 00 00 00 64 <Left>8<Right>std::string<Left>16<Left>Hoge<Left>32";
-  vector<string> each = OD::Split(input, "<Right>");
-  for(auto e:each) {
-    vector<string> fields = OD::Split(e,"<Left>");
-    auto type  = fields[0];
-    auto posi  = int64_t(stoi(fields[1]));
-    auto rawVec = OD::Split(fields[2], " ");
-    auto size = stoi(fields[3]);
-    
-    auto sample = Sample{0, 0.0, ""};
-    if ( type == "int" ) {
-      char charStack[rawVec.size()-2];
-      int cur = 0;
-      for( auto raw: rawVec) {
-        //cout << raw << endl;
-        charStack[cur] = char(stoi(raw));
-        cur ++;
-      }
-      int rec = *(int*)(&charStack);   
-      cout << "recovered int " << rec << " posi " << posi << endl;
-      *(int*)(int64_t(&sample) + posi) = rec; // recover here
+// シリアライズとデシリアライズテスト
+void testSerialDesrial() {
+  auto sa1 = Sample{1, 2.0, "Hoge"};
+  auto serial = sa1.serial();
+  cout << "SERIAL: " << serial << endl;
 
-      cout << "fullfill" << sample.a << endl;
-    }
-    if ( type == "double" ) {
-      char charStack[rawVec.size()-1];
-      int cur = 0;
-      for( auto raw: rawVec) {
-        //cout << raw << endl;
-        charStack[cur] = char(stoi(raw));
-        cur ++;
-      }
-      double rec = *(double*)(&charStack);   
-      cout << "recovered double " << rec << " posi " << posi << endl;
-      *(double*)(int64_t(&sample) + posi) = rec; // recover here
-      cout << "fullfill " << sample.b << endl;
-    }
-    if ( type == "std::string" ) {
-      std::string rec = std::string(fields[2]);
-      cout << "recovered hoge " << rec << " posi " <<  posi << endl;
-      for(auto c:rec) { 
-        cout << "char " << c << endl;
-      }
-      *(std::string*)(int64_t(&sample) + posi) = rec; // recover here
-      cout << "fullfill" << sample.c << endl;
-    }
-
-  }
+  auto sa = OD::DESERIAL::recover<Sample>(serial);
+  cout << "desrialized " << sa.a << " " << sa.b << " " << sa.c << " ," <<  endl;
 }
 
 int main() {
@@ -313,11 +243,8 @@ int main() {
   iv >> concurrent::mapper<int,int>( [](auto i){
     return i;
   }) >> echo<int>() ;
-  
-  auto sa1 = Sample{1, 2.0, "Hoge"};
-  sa1.posiParam();
-
-  recoverTest();
+ 
+  testSerialDesrial();
   /* std::queue<int> que;
   for(auto i:iv) que.push(i);
   // check CPU status
